b4a17fb1156896ef9e45b00d6b5f2aa4a5cea211
anasayrout <122938038+anasayrout@users.noreply.github.com>
2023-10-25 20:16:04 +0300

    some modifications

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-document.getElementById('toggle-password').addEventListener('click', function() {
-  let passwordInput = document.getElementById('password-input');
-  let typeAttribute = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-  passwordInput.setAttribute('type', typeAttribute);
-});
+// document.getElementById('toggle-password').addEventListener('click', function() {
+//   let passwordInput = document.getElementById('password-input');
+//   let typeAttribute = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+//   passwordInput.setAttribute('type', typeAttribute);
+// });
+
+(function($) {
+
+  $(".toggle-password").click(function() {
+
+      $(this).toggleClass("zmdi-eye zmdi-eye-off");
+      var input = $($(this).attr("toggle"));
+      if (input.attr("type") == "password") {
+        input.attr("type", "text");
+      } else {
+        input.attr("type", "password");
+      }
+    });
+
+})(jQuery);
